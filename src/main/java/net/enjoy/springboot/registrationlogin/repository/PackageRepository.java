6e16fb2c52fc60b64ede2cfa65ce998f372127e4
黄敬佩 <hjp9221@163.com>
2026-02-08 02:50:11 +0800

    把目录src\main\java\net\enjoy\springboot\registrationlogin替换为src\main\java\com\graddu\rilaserver

--- src/main/java/net/enjoy/springboot/registrationlogin/repository/PackageRepository.java
+++
@@ -1,7 +0,0 @@
-package net.enjoy.springboot.registrationlogin.repository;
-
-import net.enjoy.springboot.registrationlogin.entity.Package;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PackageRepository extends JpaRepository<Package, Long> {
-} 
